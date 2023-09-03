42ac8b54a4dc5fb6e60472c0f874d96585a2011c
pubranko <mikuras3@outlook.com>
2023-09-03 20:34:03 +0900

    設定部分の微調整

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ from decouple import config, AutoConfig
 # config = AutoConfig(search_path="./shared")
 
 # Azure Storage設定
-AZURE_STORAGE__CONNECTION_STRING: str = str(config('AZURE_STORAGE__CONNECTION_STRING'))
+AZURE_STORAGE__CONNECTION_STRING: str = str(config('AZURE_STORAGE__CONNECTION_STRING', default=''))
 AZURE_STORAGE__FILE_SHARE:str = str(config('AZURE_STORAGE__FILE_SHARE', default='controller'))
-AZURE_STORAGE_QUE__NAME:str = str(config('AZURE_QUE__NAME'))
+AZURE_STORAGE_QUE__NAME:str = str(config('AZURE_QUE__NAME', default=''))
 AZURE_STORAGE_BLOB__CONTAINER_NAME:str = str(config('AZURE_STORAGE_BLOB__CONTAINER_NAME', default='brownie-atelier'))
 AZURE_STORAGE_BLOB__FILE_NAME:str = str(config('AZURE_STORAGE_BLOB__FILE_NAME', default='container-stop-coomand-execute'))
